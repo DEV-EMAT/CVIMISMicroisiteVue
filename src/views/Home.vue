23a94f779bdf8e0763c6c1f223b495533879c628
Reymart Albor <91045838+DEV-EMAT@users.noreply.github.com>
2022-02-01 13:49:14 +0800

    session solved

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -15,7 +15,7 @@
 // import HelloWorld from "../components/HelloWorld";
 // import Dashboard from "../components/Dashboard";
 import { mapGetters } from "vuex";
-// import { LOGOUT } from "../store/auth";
+import { LOGOUT } from "../store/auth";
 
 export default {
   name: "Home",
@@ -24,33 +24,30 @@ export default {
     // if (!this.isAuthenticated) {
     //   this.$router.push({ name: "login" });
     // }
-    // var hours = 24; // Reset when storage is more than 24hours
-    // var now = new Date().getTime();
-    // var setupTime = localStorage.getItem("setupTime");
+    var hours = 24; // Reset when storage is more than 24hours
+    var now = new Date().getTime();
+    var setupTime = localStorage.getItem("setupTime");
+    console.log(setupTime);
+    if (setupTime == null) {
+      // localStorage.setItem("setupTime", now);
+      this.$router.push({ name: "login" });
+    } else {
+      if (now - setupTime > hours * 60 * 60 * 1000) {
+        localStorage.clear();
+        this.$store.dispatch(LOGOUT).then(() => {
+          this.$router.push({ name: "login" });
+          //             this.$swal(
+          //   "Logout",
+          //   "You have been successfully logged out.",
+          //   "success"
+          // );
+        });
+      } else {
         if (!this.isAuthenticated) {
           this.$router.push({ name: "login" });
         }
-    //console.log(setupTime);
-    // if (setupTime == null) {
-    //   // localStorage.setItem("setupTime", now);
-    //   this.$router.push({ name: "login" });
-    // } else {
-    //   if (now - setupTime > hours * 60 * 60 * 1000) {
-    //     localStorage.clear();
-    //     this.$store.dispatch(LOGOUT).then(() => {
-    //       this.$router.push({ name: "login" });
-    //       //             this.$swal(
-    //       //   "Logout",
-    //       //   "You have been successfully logged out.",
-    //       //   "success"
-    //       // );
-    //     });
-    //   } else {
-    //     if (!this.isAuthenticated) {
-    //       this.$router.push({ name: "login" });
-    //     }
-    //   }
-    // }
+      }
+    }
   },
   computed: {
     ...mapGetters(["isAuthenticated"]),
